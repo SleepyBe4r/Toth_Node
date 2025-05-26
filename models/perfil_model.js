5f9037632d06b11da80875d288e871164a529233
Paulo <paulohmts16@gmail.com>
2025-05-26 00:00:13 -0300

    adicionado perfil do professor e admin

--- models/perfil_model.js
+++ models/perfil_model.js
@@ -1,8 +1,8 @@
 const Database = require("../utils/database");
 
 class perfil_Model {
-    #cpf;
     #nome;
+    #cpf;
     #email;
     #telefone;
     #rua;
@@ -10,10 +10,11 @@ class perfil_Model {
     #ano_letivo;
     #serie;
     #turma;
+    #titulacao;
 
-    constructor(cpf, nome, email, telefone, rua, bairro, ano_letivo, serie, turma) {
-        this.#cpf = cpf;
+    constructor(nome, cpf, email, telefone, rua, bairro, ano_letivo, serie, turma, titulacao) {
         this.#nome = nome;
+        this.#cpf = cpf;
         this.#email = email;
         this.#telefone = telefone;
         this.#rua = rua;
@@ -21,16 +22,8 @@ class perfil_Model {
         this.#ano_letivo = ano_letivo;
         this.#serie = serie;
         this.#turma = turma;
+        this.#titulacao = titulacao;
 
-
-    }
-
-    get cpf() {
-        return this.#cpf;
-    }
-
-    set cpf(value) {
-        this.#cpf = value;
     }
 
     get nome() {
@@ -39,6 +32,14 @@ class perfil_Model {
 
     set nome(value) {
         this.#nome = value;
+    }
+
+    get cpf() {
+        return this.#cpf;
+    }
+
+    set cpf(value) {
+        this.#cpf = value;
     }
 
     get email() {
@@ -73,7 +74,7 @@ class perfil_Model {
         this.#bairro = value;
     }
 
-     get ano_letivo() {
+    get ano_letivo() {
         return this.#ano_letivo;
     }
 
@@ -81,7 +82,7 @@ class perfil_Model {
         this.#ano_letivo = value;
     }
 
-     get serie() {
+    get serie() {
         return this.#serie;
     }
 
@@ -97,9 +98,17 @@ class perfil_Model {
         this.#turma = value;
     }
 
+    get titulacao() {
+        return this.#titulacao;
+    }
+
+    set titulacao(value) {
+        this.#titulacao = value;
+    }
+
     async obterDadosAluno(cpf) {
         const sql = `
-        SELECT a.cpf_aluno, p.nome, p.email, p.telefone, p.rua, p.bairro, an.ano_letivo, s.serie, t.turma
+        SELECT p.nome, a.cpf_aluno, p.email, p.telefone, p.rua, p.bairro, an.ano_letivo, s.serie, t.turma
         FROM alunos a
         INNER JOIN pessoas p ON p.cpf = a.cpf_aluno
         INNER JOIN matriculas m ON m.cpf_aluno = a.cpf_aluno
@@ -112,10 +121,11 @@ class perfil_Model {
         const db = new Database();
         const dados = [];
         const rows = await db.ExecutaComando(sql, [cpf]);
+
         for (let i = 0; i < rows.length; i++) {
             dados.push(new perfil_Model(
-                rows[i]["cpf_aluno"],
                 rows[i]["nome"],
+                rows[i]["cpf_aluno"],
                 rows[i]["email"],
                 rows[i]["telefone"],
                 rows[i]["rua"],
@@ -123,21 +133,69 @@ class perfil_Model {
                 rows[i]["ano_letivo"],
                 rows[i]["serie"],
                 rows[i]["turma"]
-
             ));
-
         }
 
         return dados;
     }
 
     async obterDadosProfessor(cpf) {
+        const sql = `
+        SELECT p.nome, p.cpf, p.email, p.telefone, p.rua, p.bairro, pro.titulacao
+        FROM professores pro
+        INNER JOIN pessoas p ON p.cpf = pro.cpf_professor
+        WHERE pro.cpf_professor = ?
+    `;
 
-       
+        const db = new Database();
+        const dados = [];
+        const rows = await db.ExecutaComando(sql, [cpf]);
+
+        for (let i = 0; i < rows.length; i++) {
+            dados.push(new perfil_Model(
+                rows[i]["nome"],
+                rows[i]["cpf"],
+                rows[i]["email"],
+                rows[i]["telefone"],
+                rows[i]["rua"],
+                rows[i]["bairro"],
+                null,
+                null,
+                null,
+                rows[i]["titulacao"]
+            ));
+        }
+
+        return dados;
     }
 
     async obterDadosAdmin(cpf) {
-       
+        const sql = `
+        SELECT p.nome, p.cpf, p.email, p.telefone, p.rua, p.bairro
+        FROM pessoas p
+        WHERE p.cpf = ?
+    `;
+
+        const db = new Database();
+        const rows = await db.ExecutaComando(sql, [cpf]);
+        const dados = [];
+
+        for (let i = 0; i < rows.length; i++) {
+            dados.push(new perfil_Model(
+                rows[i]["nome"],
+                rows[i]["cpf"],
+                rows[i]["email"],
+                rows[i]["telefone"],
+                rows[i]["rua"],
+                rows[i]["bairro"],
+                null,
+                null,
+                null,
+                null
+            ));
+        }
+
+        return dados;
     }
 }
 
