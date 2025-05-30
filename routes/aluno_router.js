b76579a6985059df1af18c3775ee9f855d28f93d
Paulohmts <paulohmts16@gmail.com>
2025-05-29 21:21:29 -0300

    corrigido home do aluno

--- routes/aluno_router.js
+++ routes/aluno_router.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const Aluno_Controller = require("../controllers/aluno_controller");
 const Autenticacao = require("../middlewares/autenticacao");
-
+const perfil_Controller = require("../controllers/perfil_controller")
 const router = express.Router();
 
+let perfil_C = new perfil_Controller()
 let autentic = new Autenticacao();
 let aluno_C = new Aluno_Controller();
-router.get("/home", autentic.validar_Aluno, aluno_C.home_view);
+router.get("/home", autentic.validar_Aluno, perfil_C.aluno_Perfil);
 router.get("/", autentic.validar_Admin, aluno_C.listar);
 router.get("/cadastrar", autentic.validar_Admin, aluno_C.cadastrar_view);
 router.post("/cadastrar", autentic.validar_Admin, aluno_C.cadastrar_aluno);
