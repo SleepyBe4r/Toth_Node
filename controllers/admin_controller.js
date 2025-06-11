84b0029de31fc0a40cd227cb830298b436c589eb
Felipe Ferreira Carro <felipecarro02@gmail.com>
2025-06-11 16:21:49 -0300

    Tirei o JS errado

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -5,44 +5,46 @@ const Login_Model = require('../models/login_model');
 class Admin_Controller {
     home_view(req, resp){
         resp.render("admin/admin_home_view.ejs", { layout: "layout_admin_home.ejs"});
-    }
-
-    async excluir_Prof(req, resp) {
+    }    async excluir_Prof(req, resp) {
         try {
             let cpf = req.body.cpf;
             
-            // Excluir login primeiro
+            // Verificar se o professor existe
+            let professor = new Professor_Model();
+            professor.cpf = cpf;
+            let dadosProfessor = await professor.obter(cpf);
+            if (!dadosProfessor) {
+                return resp.send({ ok: false, msg: "Professor não encontrado" });
+            }
+            
+            // Excluir login primeiro (para manter integridade referencial)
             let login = new Login_Model();
             login.pessoa_cpf = cpf;
             await login.excluir();
             
             // Excluir professor
-            let professor = new Professor_Model();
-            professor.cpf = cpf;
             let resultadoProfessor = await professor.excluir();
+            if (!resultadoProfessor) {
+                return resp.send({ ok: false, msg: "Erro ao excluir professor" });
+            }
             
             // Excluir pessoa
             let pessoa = new Pessoa_Model();
             pessoa.cpf = cpf;
             let resultadoPessoa = await pessoa.excluir();
+            if (!resultadoPessoa) {
+                return resp.send({ ok: false, msg: "Erro ao excluir dados pessoais do professor" });
+            }
             
+            // Sucesso
             if (resultadoProfessor && resultadoPessoa) {
-                resp.send(`<script>
-                    alert('Professor excluído com sucesso!');
-                    window.location.href = '/admin/professores';
-                </script>`);
+                resp.send({ ok: true, msg: "Professor excluído com sucesso!" });
             } else {
-                resp.send(`<script>
-                    alert('Erro ao excluir professor');
-                    window.location.href = '/admin/professores';
-                </script>`);
+                resp.send({ ok: false, msg: "Erro ao excluir professor" });
             }
         } catch (error) {
-            console.error(error);
-            resp.send(`<script>
-                alert('Erro ao excluir professor: ${error.message}');
-                window.location.href = '/admin/professores';
-            </script>`);
+            console.error("Erro ao excluir professor:", error);
+            resp.send({ ok: false, msg: "Erro ao excluir professor: " + error.message });
         }
     }
 }
